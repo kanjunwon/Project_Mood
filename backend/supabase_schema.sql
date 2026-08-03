5a530c8758517072ffa1ec2d9d9a17516ce6b0c6
kanjunwon <junwon.k.dev@gmail.com@example.com>
2026-08-03 13:13:50 +0900

    깃허브 액션 테스트

--- backend/supabase_schema.sql
+++ backend/supabase_schema.sql
@@ -17,6 +17,6 @@ create table diary_entries (
 -- user_id로 자주 조회할 거라 인덱스 걸어둠
 create index idx_diary_entries_user_id on diary_entries(user_id);
 
--- (참고) 백엔드 repository 코드가 지금 이 컬럼명 그대로 쓰고 있음:
+-- (참고) 백엔드 repository 코드가 지금 이 컬럼명 그대로 쓰고 있음
 -- user_id, what, why, who, when_, where_, generated_diary
 -- 컬럼명 바꾸고 싶으면 backend 쪽 diary_repository.py도 같이 맞춰서 고쳐야 함
